4627ea12ab1111ea65f5508258d3b355decff57c
Đinh Vĩnh Lộc <loc.dv@vn-cubesystem.com>
2025-03-13 16:42:09 +0700

    add api basic

---
+++ Movie24h_API/Data/Movie24hContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movie24h_API.Model;
+
+namespace Movie24h_API.Data {
+    public class Movie24hContext : DbContext {
+        protected readonly IConfiguration Configuration;
+        public Movie24hContext(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Member> Members { get; set; }
+    }
+}
---
+++ Movie24h_API/Model/Member.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie24h_API.Model {
+    public class Member {
+        [Key][MaxLength(36)]
+        public required string Id { get; set; }
+        [MaxLength(100)]
+        public string? FullName { get; set; }
+        [MaxLength(200)]
+        public string? Address { get; set; }
+        public DateOnly? Birthday { get; set; }
+        [MaxLength(10)]
+        public string? Gender { get; set; }
+        [MaxLength(200)]
+        public string? Avatar { get; set; }
+        [MaxLength(12)]
+        public string? PhoneNumber { get; set; }
+        [MaxLength(200)]
+        public string? Email { get; set; }
+        [MaxLength(20)]
+        public string? Fax { get; set; }
+        [MaxLength(12)]
+        public string? CCCD { get; set; }
+        public int? ProvineId { get; set; }
+        public int? DistrictId { get; set; }
+        public int? WardId { get; set; }
+        public DateTime? CreateDate { get; set; }
+        [MaxLength(36)]
+        public string? CreateById { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        [MaxLength(36)]
+        public string? UpdateById { get; set; }
+    }
+}
